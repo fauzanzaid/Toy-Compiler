91e9737ee4b77004d3f88bc9a30c075adc280fb9
fauzanzaid <fauzanzaid.dev@gmail.com>
2018-02-06 20:12:13 +0530

    Add function to add hashtable entries, parse token strings to extract information

--- src/Lexer_init.c
+++ src/Lexer_init.c
@@ -1,8 +1,87 @@
-#include "Lexer_init.h"
+#include <string.h>
+#include <stdlib.h>
+
 #include "Lexer.h"
 #include "Token.h"
+#include "HashTable.h"
 #include "Token_Data.h"
+#include "Lexer_init.h"
 #include "Dfa_init.h"
+
+
+///////////////
+// Constants //
+///////////////
+
+int KEYWORD_INT_VALUE = TOKEN_KW_INT;
+int KEYWORD_REAL_VALUE = TOKEN_KW_REAL;
+int KEYWORD_STRING_VALUE = TOKEN_KW_STRING;
+int KEYWORD_MATRIX_VALUE = TOKEN_KW_MATRIX;
+int KEYWORD_FUNCTION_VALUE = TOKEN_KW_FUNCTION;
+int KEYWORD_END_VALUE = TOKEN_KW_END;
+int KEYWORD_IF_VALUE = TOKEN_KW_IF;
+int KEYWORD_ELSE_VALUE = TOKEN_KW_ELSE;
+int KEYWORD_ENDIF_VALUE = TOKEN_KW_ENDIF;
+int KEYWORD_READ_VALUE = TOKEN_KW_READ;
+int KEYWORD_PRINT_VALUE = TOKEN_KW_PRINT;
+int KEYWORD_MAIN_VALUE = TOKEN_KW_MAIN;
+
+char KEYWORD_INT_KEY[] = "int";
+char KEYWORD_REAL_KEY[] = "real";
+char KEYWORD_STRING_KEY[] = "string";
+char KEYWORD_MATRIX_KEY[] = "matrix";
+char KEYWORD_FUNCTION_KEY[] = "function";
+char KEYWORD_END_KEY[] = "end";
+char KEYWORD_IF_KEY[] = "if";
+char KEYWORD_ELSE_KEY[] = "else";
+char KEYWORD_ENDIF_KEY[] = "endif";
+char KEYWORD_READ_KEY[] = "read";
+char KEYWORD_PRINT_KEY[] = "print";
+char KEYWORD_MAIN_KEY[] = "_main";
+
+
+///////////////
+// Hashtable //
+///////////////
+
+HashTable *keyword_table;
+
+static int string_hash_function(void *ptr){
+	char *string = (char *)ptr;
+	int hash = 5381;
+	int c;
+	while(c = *string++)
+		hash = ((hash << 5) + hash) + c;
+
+	if(hash<0)
+		return -hash;
+	else
+		return hash;
+}
+
+static int string_key_compare(void *ptr_a, void *ptr_b){
+	return strcmp((char *)ptr_a, (char *)ptr_b);
+}
+
+void keyword_table_init(){
+	keyword_table = HashTable_new(13, string_hash_function, string_key_compare);
+
+	HashTable_add(keyword_table, (void *)&KEYWORD_INT_KEY, (void *)&KEYWORD_INT_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_REAL_KEY, (void *)&KEYWORD_REAL_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_STRING_KEY, (void *)&KEYWORD_STRING_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_MATRIX_KEY, (void *)&KEYWORD_MATRIX_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_FUNCTION_KEY, (void *)&KEYWORD_FUNCTION_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_END_KEY, (void *)&KEYWORD_END_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_IF_KEY, (void *)&KEYWORD_IF_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_ELSE_KEY, (void *)&KEYWORD_ELSE_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_ENDIF_KEY, (void *)&KEYWORD_ENDIF_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_READ_KEY, (void *)&KEYWORD_READ_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_PRINT_KEY, (void *)&KEYWORD_PRINT_VALUE);
+	HashTable_add(keyword_table, (void *)&KEYWORD_MAIN_KEY, (void *)&KEYWORD_MAIN_VALUE);
+}
+
+
+
 
 ///////////////
 // Functions //
@@ -63,33 +142,74 @@ void success_evaluate_function(Token *tkn_ptr, int state, char *string, int len_
 	}
 
 	else if(state == STATE_F_ID_OR_KW){
-		tkn_ptr->type = TOKEN_UNKOWN;
-		// 
+		tkn_ptr->data->len_string = len_string;
+		
+		tkn_ptr->data->string = malloc( sizeof(char) * (1 + len_string) );
+		memcpy(tkn_ptr->data->string, string, len_string);
+		tkn_ptr->data->string[len_string] = '\0';
+
+		Token_type *tkn_type_ptr = HashTable_get(keyword_table, tkn_ptr->data->string);
+		if(tkn_type_ptr == NULL)
+			tkn_ptr->type = TOKEN_ID;
+		else
+			tkn_ptr->type = *tkn_type_ptr;
 	}
 
 	else if(state == STATE_F_ID){
 		tkn_ptr->type = TOKEN_ID;
-		// 
+
+		tkn_ptr->data->len_string = len_string;
+		
+		tkn_ptr->data->string = malloc( sizeof(char) * (1 + len_string) );
+		memcpy(tkn_ptr->data->string, string, len_string);
+		tkn_ptr->data->string[len_string] = '\0';
+
 	}
 
 	else if(state == STATE_F_FUNID_OR_KW){
-		tkn_ptr->type = TOKEN_UNKOWN;
-		// 
+		tkn_ptr->data->len_string = len_string;
+		
+		tkn_ptr->data->string = malloc( sizeof(char) * (1 + len_string) );
+		memcpy(tkn_ptr->data->string, string, len_string);
+		tkn_ptr->data->string[len_string] = '\0';
+
+
+		Token_type *tkn_type_ptr = HashTable_get(keyword_table, tkn_ptr->data->string);
+		if(tkn_type_ptr == NULL)
+			tkn_ptr->type = TOKEN_FUNID;
+		else
+			tkn_ptr->type = *tkn_type_ptr;
 	}
 
 	else if(state == STATE_F_NUM){
 		tkn_ptr->type = TOKEN_NUM;
-		// 
+		
+		char tmp[1+len_string];
+		memcpy(tmp, string, len_string);
+		tmp[len_string] = '\0';
+
+		tkn_ptr->data->integer = atoi(tmp);
 	}
 
 	else if(state == STATE_F_RNUM){
 		tkn_ptr->type = TOKEN_RNUM;
-		// 
+
+		char tmp[1+len_string];
+		memcpy(tmp, string, len_string);
+		tmp[len_string] = '\0';
+
+		tkn_ptr->data->integer = atoi(tmp);
+		tkn_ptr->data->fraction = atoi(&tmp[len_string - 2]);
 	}
 
 	else if(state == STATE_F_STR){
 		tkn_ptr->type = TOKEN_STR;
-		// 
+
+		int len_literal = len_string-2;	// Remove the two "
+
+		tkn_ptr->data->string = malloc( sizeof(char) * (1 + len_literal) );
+		memcpy(tkn_ptr->data->string, string+1, len_literal);
+		tkn_ptr->data->string[len_literal] = '\0';
 	}
 
 	else if(state == STATE_F_AND){

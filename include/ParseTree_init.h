7399690c28b8c7ff37a6c6fc456ba4cdc68b0362
fauzanzaid <fauzanzaid.dev@gmail.com>
2018-04-07 19:05:01 +0530

    Implement function to prune parse tree. Implement Operattor_type enum

--- include/ParseTree_init.h
+++ include/ParseTree_init.h
@@ -8,12 +8,37 @@
 // Types //
 ///////////
 
+typedef enum {
+	OPERATOR_STMT_OR_DEF_LIST = 1,
+	OPERATOR_DECL,
+	OPERATOR_ID_LIST,
+	OPERATOR_TYPE,
+	OPERATOR_ASSIGN,
+	OPERATOR_MATRIX_ROW_LIST,
+	OPERATOR_MATRIX_NUM_LIST,
+	OPERATOR_CALL,
+	OPERATOR_BRANCH,
+	OPERATOR_STMT_LIST,
+	OPERATOR_DEF,
+	OPERATOR_PARAM_LIST,
+	OPERATOR_INDEX,
+}Operator_type;
+
 typedef struct ParseTree_Node_Attr
 {
+	int op;
+	void *type;	int type_valid;
 	void *inh;	int inh_valid;
 	void *syn;	int syn_valid;
 	void *code;	int code_valid;
 } ParseTree_Node_Attr;
+
+
+///////////////
+// Functions //
+///////////////
+
+ParseTree_Node *prune_parse_tree(ParseTree_Node *tree_ptr);
 
 
 #endif
